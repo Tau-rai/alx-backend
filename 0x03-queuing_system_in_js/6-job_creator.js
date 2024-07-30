0a2ab4b4d04271908084682612eaf1bd0c8a457b
Tau <taumasaire@gmail.com>
2024-07-30 18:50:53 +0200

    adding the job processor script

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,21 @@
+// creates a job using kue
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+const jobData = {
+    phoneNumber: '4567890',
+    message: 'Job notification',
+  }
+
+const job = queue.create('push_notification_code', jobData).save((err) => {
+    if (!err) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => {
+    console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+    console.log('Notification job failed');
+});
---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,13 @@
+// creates a job processor for push_notification_code
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+queue.process('push_notification_code', (job, done) => {
+    sendNotification(job.data.phoneNumber, job.data.message);
+    done();
+});
